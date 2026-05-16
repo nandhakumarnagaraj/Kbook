7cf29707c5ce3c4e293ec8eb4728c36738be048b
nandhakumarnagaraj <nandhakumar2536@gmail.com>
2026-05-17 02:34:09 +0530

    chore: bump default version to 1.0.8 (15) for v1 release

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -41,8 +41,8 @@ val signingStoreFile = configValue("SIGNING_STORE_FILE")
 val signingStorePassword = configValue("SIGNING_STORE_PASSWORD")
 val signingKeyAlias = configValue("SIGNING_KEY_ALIAS")
 val signingKeyPassword = configValue("SIGNING_KEY_PASSWORD")
-val releaseVersionCode = configValue("RELEASE_VERSION_CODE", "1").toIntOrNull() ?: 1
-val releaseVersionName = configValue("RELEASE_VERSION_NAME", "1.0.0")
+val releaseVersionCode = configValue("RELEASE_VERSION_CODE", "15").toIntOrNull() ?: 15
+val releaseVersionName = configValue("RELEASE_VERSION_NAME", "1.0.8")
 val hasReleaseSigning =
     signingStoreFile.isNotBlank() &&
         signingStorePassword.isNotBlank() &&
